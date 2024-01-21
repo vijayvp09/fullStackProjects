59682791b1ceed2d742f78eac27d6c746aa1a775
vijayvp09 <vvpillai95@gmail.com>
2024-01-21 22:33:53 +0530

    created route to update existing book

--- bookstore/backend/index.js
+++ bookstore/backend/index.js
@@ -63,6 +63,33 @@ app.get('/books/:id', async (req, res) => {
         res.status(500).send({message: error.message});
     }
 });
+//Route for update a book
+app.put('/books/:id', async (req, res) => {
+    try{
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ) {return response.status(400).send({
+            message: 'send all required fields: title, author, publishYear',
+        });
+    }
+
+    const {id} = req.params;
+
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+        return res.status(404).json({message: 'Book not found'});
+    }
+
+    return res.status(200).send({message: 'Book updated successfully'});
+    }catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message})
+    }
+});
+
 mongoose
 .connect(mongoDBurl)
 .then(()=>{
