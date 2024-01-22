2d2f1a75fab409fa819ac98360dafc4698b6c570
vijayvp09 <vvpillai95@gmail.com>
2024-01-22 23:00:59 +0530

    created a route for delete book and tested using postman

--- bookstore/backend/index.js
+++ bookstore/backend/index.js
@@ -90,6 +90,20 @@ app.put('/books/:id', async (req, res) => {
     }
 });
 
+app.delete('/books/:id', async (req, res) => {
+    try {
+        const {id} =req.params;
+        const result = await Book.findByIdAndDelete(id);
+
+        if(!result) {
+            return res.status(404).json({ message: 'Book not found'});
+        }
+        return res.status(200).send({message: 'Book deleted successfully'});
+    }catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message});
+    }
+})
 mongoose
 .connect(mongoDBurl)
 .then(()=>{
