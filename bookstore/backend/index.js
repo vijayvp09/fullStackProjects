1bf9ca8371ff529a4d8c057efcdab2c0fbd700f1
vijayvp09 <vvpillai95@gmail.com>
2024-01-27 21:05:07 +0530

    cors added

--- bookstore/backend/index.js
+++ bookstore/backend/index.js
@@ -3,10 +3,21 @@ import {PORT, mongoDBurl} from "./config.js";
 import mongoose from "mongoose"
 import {Book} from "./models/bookModel.js"
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+//app.use(cors());
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+)
 app.get("/", (req, res) => {
     console.log(req);
     return res.status(211).send("Welcome to MERN project")
