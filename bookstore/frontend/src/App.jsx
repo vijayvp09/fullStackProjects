3ed38280bd850db087da1ee6915f9e91ae5ef3c5
vijayvp09 <vvpillai95@gmail.com>
2024-01-29 22:03:49 +0530

    edited setup and added tailwind and tested setup

--- bookstore/frontend/src/App.jsx
+++ bookstore/frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      
+      <h1 className='bg-red-300 text-black'>hello</h1>
     </>
   )
 }
