0078abeee278a91774448989287b1635b02c16f9
vijayvp09 <vvpillai95@gmail.com>
2024-02-04 19:31:22 +0530

    started develping a backend server by creating a post route to handle register request

--- blog/client/src/pages/RegisterPage.jsx
+++ blog/client/src/pages/RegisterPage.jsx
@@ -1,9 +1,22 @@
+import {useState} from 'react';
+
 export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+    async function register(e) {
+        e.preventDefault();
+        await fetch("http://localhost:4000/register", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({username, password}),
+        })
+    }
     return (
-            <form action="" className="register">
+            <form action="" className="register" onSubmit={register}>
                 <h1>Register</h1>
-                <input type="text" placeholder="username" />
-                <input type="text" placeholder="password" />
+                <input type="text" placeholder="username" value={username} onChange={e => setUsername(e.target.value)} />
+                <input type="text" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
                 <button>Register</button>
             </form>
     )
