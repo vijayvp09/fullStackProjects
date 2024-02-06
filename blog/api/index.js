f82b2c1e708f530b401746fe9f8a2f209416ce12
vijayvp09 <vvpillai95@gmail.com>
2024-02-06 23:04:48 +0530

    created logic for register and login

--- blog/api/index.js
+++ blog/api/index.js
@@ -3,24 +3,45 @@ const app = express();
 const cors = require('cors');
 const User = require('./models/User');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-// mongoose.connect("mongodb+srv://vijay:krishnakaali@cluster0.lepef44.mongodb.net/?retryWrites=true&w=majority")
+const salt = bcrypt.genSaltSync(10);
+const secret = "asfgahsdaoerfgoia";
 
-app.use(cors())
-app.use(express.json())
+app.use(cors({credentials: true, origin:'http://localhost:5173'}));
+app.use(express.json());
 
 mongoose.connect("mongodb+srv://vijay:krishnakaali@cluster0.lepef44.mongodb.net/?retryWrites=true&w=majority")
 
 app.post('/register',async (req, res) => {
     const {username, password} = req.body;
     try{
-        const response = await User.create({username, password});
-        res.json(response);
+        const userDoc = await User.create({
+            username, 
+            password: bcrypt.hashSync(password, salt),
+        });
+        res.json(userDoc);
     } catch(e) {
-        console.error(e)
-        res.status(400)
-    }
+        console.log(e);
+        res.status(400).json(e);
+    }    
+});
+app.post("/login", async (req,res) => {
+    const {username, password} = req.body;
+    try{
+        const userDoc = await User.findOne({username});
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if(passOk) {
+            jwt.sign({username,id:userDoc._id}, secret, {},(err, token)=>{
+                if(err) throw err;
+                res.cookie("token", token).json("ok");
+            });
+        }
+    }catch{
     
+        res.status(400).json("wrong credentials")
+    }
 });
 
 app.listen(4000);
--- blog/client/src/pages/LoginPage.jsx
+++ blog/client/src/pages/LoginPage.jsx
@@ -1,9 +1,35 @@
+import {useState} from 'react';
+import {Navigate} from 'react-router-dom';
+
 export default function LoginPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+    async function login(e) {
+        e.preventDefault();
+        const response = await fetch("http://localhost:4000/login", {
+            method: "POST",
+            headers: {"Content-Type":'application/json'},
+            body: JSON.stringify({username, password}),
+            credentials: 'include',
+        })
+        if(response.ok){
+            setRedirect(true);
+            //login
+        }else{
+            alert("wrong, credentials")
+        }
+    }
+    
+    if(redirect) {
+        return <Navigate to={'/'} />
+    }
+    
     return (
-            <form action="" className="login">
+            <form action="" className="login" onSubmit={login} >
                 <h1>Login</h1>
-                <input type="text" placeholder="username" />
-                <input type="text" placeholder="password" />
+                <input type="text" placeholder="username" value={username} onChange={e => setUsername(e.target.value)} />
+                <input type="text" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
                 <button>Login</button>
             </form>
     )
