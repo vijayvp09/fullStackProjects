87b955821cf21d39520e1bcc74ba3fd6a8a2c775
vijayvp09 <vvpillai95@gmail.com>
2024-02-14 23:56:18 +0530

    create post logic

--- blog/client/src/pages/CreatePost.jsx
+++ blog/client/src/pages/CreatePost.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+import { useState } from 'react';
+
+const modules = {};
+const formats = [
+    "header","font","size","bold","italic",
+    "underline","strike","blockquote","list","bullet","indent",
+    "link","image"
+];
 
 export default function CreatePost() {
+    const [title, setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [content, setContent] = useState('');
+    
     return(
-        <div>
-            here you create your new POST
-        </div>
+        <form>
+            <input type="title" placeholder={'Title'} />
+            <input type="summary" placeholder={'Summary'} />
+            <input type="file" />
+            <ReactQuill formats={formats} />
+            <button style={{marginTop:'5px'}}>Create Post</button>
+        </form>
     )
 }
