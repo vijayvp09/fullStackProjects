6963b3bd8db9f95a035144b02985efde865db680
vijayvp09 <vvpillai95@gmail.com>
2024-02-15 12:08:15 +0530

    create post frontend logic

--- blog/api/index.js
+++ blog/api/index.js
@@ -56,6 +56,9 @@ app.get('/profile', (req, res) => {
 });
 app.post('/logout', (req, res) => {
     res.cookie('token', '').json('ok')
-})
+});
+app.post('/post', (req, res) => {
+
+});
 
 app.listen(4000);
--- blog/client/src/pages/CreatePost.jsx
+++ blog/client/src/pages/CreatePost.jsx
@@ -2,7 +2,18 @@ import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 import { useState } from 'react';
 
-const modules = {};
+const modules = {
+    toolbar: [
+    ['bold', 'italic', 'underline', 'strike'],
+
+    [{ list: 'ordered'}, { list: 'bullet' }],
+    [{ indent: '-1'}, { indent: '+1' }],
+
+    [{ header: [1, 2, false] }],
+    ['link', 'image', 'video'],
+    ['clean'],
+  ]};
+
 const formats = [
     "header","font","size","bold","italic",
     "underline","strike","blockquote","list","bullet","indent",
@@ -13,13 +24,25 @@ export default function CreatePost() {
     const [title, setTitle] = useState('');
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
-    
+    const [files, setFiles] = useState('');
+    function createNewPost(e) {
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0]);
+        e.preventDefault();
+        fetch("http://localhost:4000/post", {
+            method: 'POST',
+            body: data,
+        })
+    }
     return(
-        <form>
-            <input type="title" placeholder={'Title'} />
-            <input type="summary" placeholder={'Summary'} />
-            <input type="file" />
-            <ReactQuill formats={formats} />
+        <form onSubmit={createNewPost}>
+            <input type="title" placeholder={'Title'} value={title} onChange={e => setTitle(e.target.value)} />
+            <input type="summary" placeholder={'Summary'} value={summary} onChange={e => setSummary(e.target.value)} />
+            <input type="file" onChange={(e => setFiles(e.target.files))} />
+            <ReactQuill modules={modules} formats={formats} value={content} onChange={newValue => setContent(newValue)} />
             <button style={{marginTop:'5px'}}>Create Post</button>
         </form>
     )
